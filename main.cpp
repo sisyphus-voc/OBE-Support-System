b46f3bf8d4a60648902212ea86b7faa0174948cc
Abdul Raheem <voc.abdulraheem@gmail.com>
2024-04-16 10:46:04 +0500

    Class declaration in header files

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <cmath>
+#include <vector>
+/// #include "includes/actors.cpp"
+// #include "header/"
+using namespace std;
+
+int main()
+{
+    //Get Teacher and Academic Officer data from file  and copy into arrays/vectors
+
+    //login the user and compare it in both arrays(redundant)
+    //add some codein username like T to signify teacher whilst A  to signify AO so you dont have to traverse both arrays
+
+    
+
+
+    
+
+    return 0;
+}
